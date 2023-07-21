9a572e6e2a1773a4799405a17f3c00e0dc5235d4
blac <badejo.semilore.7@gmail.com>
2023-07-21 12:12:25 +0100

    almost done

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import ncp from "ncp";
 import fs from "fs";
-import chalk from "chalk";
+import colors from "colors";
 import { promisify } from "util";
 import path from "path";
-import { URL } from "url";
+import { fileURLToPath } from "url";
+import shelljs from "shelljs";
+import Listr from "listr";
+import { projectInstall } from "pkg-install";
 
 const access = promisify(fs.access);
 const copy = promisify(ncp);
+
+async function initGit(options) {
+  const gitInitResult = shelljs.exec("git init", {
+    cwd: options.targetDirectory,
+  });
+
+  if (gitInitResult.code !== 0) {
+    return Promise.reject(new Error("Failed to initialize git"));
+  }
+
+  return;
+}
 
 async function copyTemplateFiles(options) {
   return copy(options.templateDirectory, options.targetDirectory, {
@@ -20,26 +35,46 @@ export async function createProject(options) {
     targetDirectory: options.targetDirectory || process.cwd(),
   };
 
-  const currentFileUrl = import.meta.url;
+  const currentFileUrl = fileURLToPath(import.meta.url);
   const templateDir = path.resolve(
-    new URL(currentFileUrl).pathname,
-    "../../templates",
+    path.dirname(currentFileUrl),
+    "../templates",
     options.template.toLowerCase()
   );
   options.templateDirectory = templateDir;
 
-  // console.log(templateDir);
-
   try {
     await access(templateDir, fs.constants.R_OK);
   } catch (err) {
-    console.error("%s Invalid template name", chalk.red.bold("ERROR"));
+    console.error("%s Invalid template name".red.bold, "ERROR");
     process.exit(1);
   }
 
-  console.log("Copying project files");
-  await copyTemplateFiles(options);
+  const tasks = new Listr([
+    {
+      title: "Copy project files",
+      task: () => copyTemplateFiles(options),
+    },
+    {
+      title: "Initialize git",
+      task: () => initGit(options),
+      enabled: () => options.git, // Use a function to determine if the task should be enabled or skipped
+    },
+    {
+      title: "Install dependencies",
+      task: () =>
+        projectInstall({
+          cwd: options.targetDirectory,
+        }),
+      skip: () =>
+        !options.runInstall
+          ? "Pass --install to automatically install development dependencies"
+          : undefined,
+    },
+  ]);
 
-  console.log("%s Project ready", chalk.green.bold("DONE"));
+  await tasks.run();
+
+  console.log("%s Project ready".green.bold, "DONE");
   return true;
 }
